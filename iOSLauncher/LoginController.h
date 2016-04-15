f85b888fd6574ab0a421223d7971fdce840dcb11
Sergio L. Pascual <sergiolpascual@flexvdi.com>
2016-04-15 14:50:47 +0200

    Add copyright notice to common and iOS headers. Include GPLv2 license (COPYING).

--- iOSLauncher/LoginController.h
+++ iOSLauncher/LoginController.h
@@ -1,10 +1,23 @@
-//
-//  LoginController.h
-//  spice_ios
-//
-//  Created by Wilfredo Carrillo on 7/24/14.
-//  Copyright (c) 2014 Wilfredo Carrillo. All rights reserved.
-//
+/*
+ * launcher-mobile: a multiplatform flexVDI/SPICE client
+ *
+ * Copyright (C) 2016 flexVDI (Flexible Software Solutions S.L.)
+ *
+ * This file is part of launcher-mobile.
+ *
+ * launcher-mobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * launcher-mobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with launcher-mobile.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import <UIKit/UIKit.h>
 
