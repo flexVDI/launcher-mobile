47d54b38878289a10684ec0e874567947ef349cf
Sergio L. Pascual <sergiolpascual@flexvdi.com>
2016-04-18 18:18:34 +0200

    iOS: Bump release version. Now at 2.2.7

--- iOSLauncher/ConfigViewController.m
+++ iOSLauncher/ConfigViewController.m
@@ -41,7 +41,7 @@
     _txtIp.delegate = self;
     _txtPort.delegate = self;
     _txtDomain.delegate = self;
-    _lblVersion.text = @"v2.2.6";
+    _lblVersion.text = @"v2.2.7";
     _lblVersion.layer.cornerRadius = 8.0f;
     
     NSString *ver = [[UIDevice currentDevice] systemVersion];
