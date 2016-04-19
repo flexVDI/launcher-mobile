0547d7c9c4b65979f91603101a9fb98212ed2d83
Sergio L. Pascual <sergiolpascual@flexvdi.com>
2016-04-19 09:58:57 +0200

    Android: add copyright notice to all source files.

--- AndroidLauncher/app/src/main/java/com/flexvdi/androidlauncher/ScaleListener.java
+++ AndroidLauncher/app/src/main/java/com/flexvdi/androidlauncher/ScaleListener.java
@@ -1,3 +1,24 @@
+/*
+ * launcher-mobile: a multiplatform flexVDI/SPICE client
+ *
+ * Copyright (C) 2016 flexVDI (Flexible Software Solutions S.L.)
+ *
+ * This file is part of launcher-mobile.
+ *
+ * launcher-mobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * launcher-mobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with launcher-mobile.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.flexvdi.androidlauncher;
 
 import android.util.Log;
